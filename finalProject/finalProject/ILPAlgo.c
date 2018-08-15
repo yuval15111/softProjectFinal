d2cef2740a157722f4c491b40e5a25fdfb687951
adieldar1 <adieldar1995@gmail.com>
2018-08-15 11:15:41 +0300

    sss

--- finalProject/finalProject/ILPAlgo.c
+++ finalProject/finalProject/ILPAlgo.c
@@ -8,34 +8,32 @@ extern solvedSudoku;
 extern blockWidth, blockHeight, N;
 
 
-void freeAll(double lb, double val, char verType, int index) {
-
-	$ free alocated memory except "sol" which free in exitILP function @
-		free(lb);
+void freeAll(double* lb, double* val, char* verType, int* index) {
+	free(lb);
 	free(val);
 	free(verType);
 	free(index);
 }
-void writeSolToBoard(double *sol) {
+
+void writeSolToBoard(double* sol) {
 	int i, j, k;
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
-			for (k = 1; k <= N; k++) {
-				if (sol[i * N * N + j * N + k - 1] == 1) {
-					solvedSudoku[i*N + j].value = k;
+			for (k = 0; k < N; k++) {
+				if (sol[i * N * N + j * N + k] == 1) {
+					solvedSudoku[i*N + j].value = k + 1;
 				}
 			}
 		}
 	}
 }
-int exitILP(GRBenv **env, GRBmodel **model, int flag, int optimStatus, double *sol, int ThreeDMatrixSize) {
-	int flagGetSol = 0;
-	if (flag == 0 && optimStatus == GRB_OPTIMAL) {
-		$ the board is solveable @
-			flagGetSol = GRBgetdblattrarray(*model, GRB_DBL_ATTR_X, 0, ThreeDMatrixSize, sol);
-		if (flagGetSol == 0) {
-			$ retrive the solution successfully @
-				writeSolToBoard(sol);
+
+int exitILP(GRBenv** env, GRBmodel** model, int flag, int optimStat, double* sol, int ThreeDMatrixSize) {
+	int solAns = 0;
+	if (flag == 0 && optimStat == GRB_OPTIMAL) { $ the board is solveable @
+		solAns = GRBgetdblattrarray(*model, GRB_DBL_ATTR_X, 0, ThreeDMatrixSize, sol);
+		if (solAns == 0) { $ retrive the solution successfully @
+			writeSolToBoard(sol);
 			free(sol);
 			GRBfreemodel(*model);
 			GRBfreeenv(*env);
@@ -46,97 +44,21 @@ int exitILP(GRBenv **env, GRBmodel **model, int flag, int optimStatus, double *s
 		GRBfreeenv(*env);
 		return -1;
 	}
-	else if (flag == 0 && optimStatus == GRB_INFEASIBLE) {
-		$ the board is unsolveable @
-			GRBfreemodel(*model);
+	else if (flag == 0 && optimStat == GRB_INFEASIBLE) { $ the board is unsolveable @
+		GRBfreemodel(*model);
 		GRBfreeenv(*env);
 		return 2;
 	}
-	else {
-		$ flag occured @
-			GRBfreemodel(*model);
+	else { $ a problem occured @
+		GRBfreemodel(*model);
 		GRBfreeenv(*env);
 		return -1;
 	}
 }
-int solInfo(GRBmodel **model, int *optimStatus) {
-	$ get the optimization status :
-	*optimStatus == GRB_OPTIMAL->board is solvable
-		* optimStatus == GRB_INFEASIBLE->board is unsolvable@
-		return GRBgetintattr(*model, GRB_INT_ATTR_STATUS, optimStatus);
-}
-int optimizemodel(GRBmodel **model) {
-	return GRBoptimize(*model);
-}
-int oneValuePerBlock(GRBmodel model, int index, double val) {
-	$ add constraint : each value is used exactly once per block @
-		int i, j, k, k, l;
-	int count, flag = 0;
-	boardData brdData = getBoardData();
-	int N = blockWidth * blockHeight;
-	for (k = 0; k < N; k++) {
-		for (k = 0; k < blockHeight; k++) {
-			for (l = 0; l < blockWidth; l++) {
-				count = 0;
-				for (i = k * blockHeight; i < (k + 1) * blockHeight; i++) {
-					for (j = l * blockWidth; j < (l + 1) * blockWidth; j++) {
-						index[count] = i * N * N + j * N + k;
-						val[count] = 1.0;
-						count++;
-					}
-				}
-				flag = GRBaddconstr(*model, N, index, val, GRB_EQUAL, 1.0, NULL);
-				if (flag) {
-					return 1;
-				}
-			}
-		}
-	}
-	return 0;
-}
-int oneValuePerCol(GRBmodel model, int index, double val) {
-	$ add constraint : each value is used exactly once per column @
+
+int oneValPerCellCon(GRBmodel model, int index, double val) {
+	$ constraint : each cell have exactly one value @
 		int i, j, k, flag = 0;
-	boardData brdData = getBoardData();
-	int N = blockWidth * blockHeight;
-	for (k = 0; k < N; k++) {
-		for (i = 0; i < N; i++) {
-			for (j = 0; j < N; j++) {
-				index[j] = i * N * N + j * N + k;
-				val[j] = 1.0;
-			}
-			flag = GRBaddconstr(*model, N, index, val, GRB_EQUAL, 1.0, NULL);
-			if (flag) {
-				return 1;
-			}
-		}
-	}
-	return 0;
-}
-int oneValuePerRow(GRBmodel model, int index, double val) {
-	$ add constraint : each value is used exactly once per row @
-		int i, j, k, flag = 0;
-	boardData brdData = getBoardData();
-	int N = blockWidth * blockHeight;
-	for (k = 0; k < N; k++) {
-		for (j = 0; j < N; j++) {
-			for (i = 0; i < N; i++) {
-				index[i] = i * N * N + j * N + k;
-				val[i] = 1.0;
-			}
-			flag = GRBaddconstr(*model, N, index, val, GRB_EQUAL, 1.0, NULL);
-			if (flag) {
-				return 1;
-			}
-		}
-	}
-	return 0;
-}
-int oneValuePerCell(GRBmodel model, int index, double val) {
-	//$ add constraint : each cell must have exactly one value @
-	int i, j, k, flag = 0;
-	boardData brdData = getBoardData();
-	int N = blockWidth * blockHeight;
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
 			for (k = 0; k < N; k++) {
@@ -152,12 +74,71 @@ int oneValuePerCell(GRBmodel model, int index, double val) {
 	return 0;
 }
 
-int newEnviroment(GRBenv **env) {
-	return GRBloadenv(env, "ILP.log");
+int oneValPerColCon(GRBmodel model, int index, double val) {
+	$ constraint : each value is used exactly once in each column @
+		int i, j, k, flag = 0;
+	for (k = 0; k < N; k++) {
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++) {
+				index[j] = i * N * N + j * N + k;
+				val[j] = 1.0;
+			}
+			flag = GRBaddconstr(*model, N, index, val, GRB_EQUAL, 1.0, NULL);
+			if (flag) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int oneValPerRowCon(GRBmodel model, int index, double val) {
+	$ constraint : each value is used exactly once in each row @
+		int i, j, k, flag = 0;
+	for (k = 0; k < N; k++) {
+		for (j = 0; j < N; j++) {
+			for (i = 0; i < N; i++) {
+				index[i] = i * N * N + j * N + k;
+				val[i] = 1.0;
+			}
+			flag = GRBaddconstr(*model, N, index, val, GRB_EQUAL, 1.0, NULL);
+			if (flag) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int oneValPerBlockCon(GRBmodel model, int index, double val) {
+	$ constraint : each value is used exactly once in each block @
+		int i, j, e, k, l;
+	int count, flag = 0;
+	for (e = 0; e < N; e++) {
+		for (k = 0; k < blockHeight; k++) {
+			for (l = 0; l < blockWidth; l++) {
+				count = 0;
+				for (i = k * blockHeight; i < (k + 1) * blockHeight; i++) {
+					for (j = l * blockWidth; j < (l + 1) * blockWidth; j++) {
+						index[count] = i * N * N + j * N + e;
+						val[count] = 1.0;
+						count++;
+					}
+				}
+				flag = GRBaddconstr(*model, N, index, val, GRB_EQUAL, 1.0, NULL);
+				if (flag) {
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
 }
 
 void addVaribles(double *lb, char *verType) {
-	int i, j, k;
+	$ if a cell value equals k->insert 1 at this index in lb matrix
+	else->insert 0 at this index in lb matrix @
+		int i, j, k;
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
 			for (k = 0; k < N; k++) {
@@ -172,6 +153,7 @@ void addVaribles(double *lb, char *verType) {
 		}
 	}
 }
+
 int ILPSolver() {
 	$ try to solve the currentSudoku using ILP.
 		* if the board is solvable it writes the solution to the tempBoard and return 1,
@@ -180,67 +162,63 @@ int ILPSolver() {
 
 			GRBmodel* model = NULL;
 	GRBenv* env = NULL;
-
-	int ThreeDMatrixSize, optimStatus, flag = 0, *index;
-	char* verType; $variable types@
-		int
-		double* lb, *val, *sol;
-
+	int ThreeDMatrixSize, optimStat, flag = 0, *index;
+	char* verType;
+	double* lb, *val, *sol;
 	ThreeDMatrixSize = N * N * N;
 	lb = (double*)malloc(ThreeDMatrixSize * sizeof(double));
 	verType = (char*)malloc(ThreeDMatrixSize * sizeof(char));
 	index = (int*)malloc(N * sizeof(int));
 	val = (double*)malloc(N * sizeof(double));
 	sol = (double*)malloc(ThreeDMatrixSize * sizeof(double));
-
 	addVaribles(lb, verType);
-	flag = newEnviroment(&env);
+	flag = GRBloadenv(env, "ILP.log");
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
 	flag = GRBsetintparam(*env, GRB_INT_PAR_LOGTOCONSOLE, 0);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
 	flag = GRBnewmodel(*env, model, "ILP", ThreeDMatrixSize, NULL, lb, NULL, verType, NULL);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
-	flag = oneValuePerCell(&model, index, val);
+	flag = oneValPerCellCon(&model, index, val);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
-	flag = oneValuePerCol(&model, index, val);
+	flag = oneValPerColCon(&model, index, val);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
-	flag = oneValuePerRow(&model, index, val);
+	flag = oneValPerRowCon(&model, index, val);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
-	flag = oneValuePerBlock(&model, index, val);
+	flag = oneValPerBlockCon(&model, index, val);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
-	flag = optimizemodel(&model);
+	flag = GRBoptimize(*model);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
-	flag = solInfo(&model, &optimStatus);
+	flag = GRBgetintattr(*model, GRB_INT_ATTR_STATUS, optimStat);
 	if (flag) {
 		freeAll(lb, val, verType, index);
-		return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+		return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 	}
 	freeAll(lb, val, verType, index);
-	return exitILP(&env, &model, flag, optimStatus, sol, ThreeDMatrixSize);
+	return exitILP(&env, &model, flag, optimStat, sol, ThreeDMatrixSize);
 }
 
 
--- finalProject/finalProject/ILPAlgo.h
+++ finalProject/finalProject/ILPAlgo.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef ILPAlgo_H_
+#define ILPAlgo_H_
+
+/*
+This function frees all memory allocations except of 'sol'
+*/
+void freeAll(double* lb, double* val, char* verType, int* index);
+
+/*
+This function writes the solution in sol to the sudoku:
+it checks for each k s.t 1<=k<=N wether sol in k index equals 1,
+if so -> insert k as the value of the same index in the sudoku
+*/
+void writeSolToBoard(double* sol);
+
+/*
+This function 
+*/
+int exitILP(GRBenv** env, GRBmodel** model, int flag, int optimStat, double* sol, int ThreeDMatrixSize);
+
+#endif /*ILPAlgo_H_*/
